78b466150d760d900e4a99133fce8ab81dcdcce5
sx1n <samuellaurindodelima2004@gmail.com>
2023-05-25 20:54:11 -0300

    read line

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,32 +1,4 @@
 fun main() {
-    println(bonus("Gerente", 2))
-}
-
-fun bonus(cargo: String, experiencia: Int): Float {
-    var bonus = 0f
-
-    when (cargo) {
-        "Gerente" -> {
-            if (experiencia < 2) {
-                bonus = 2000f
-            } else {
-                bonus = 3000f
-            }
-        }
-        "Coordenador" -> {
-            if (experiencia < 1) {
-                bonus = 1500f
-            } else {
-                bonus = 1800f
-            }
-        }
-        "Engenheiro de software" -> {
-            bonus = 1000f
-        }
-        "Estágiario" -> {
-            bonus = 500f
-        }
-    }
-
-    return bonus
+    val s = readlnOrNull() // read Line
+    println(s)
 }
