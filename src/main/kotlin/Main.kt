0aa64c5b680ff7b417fa7a1608397a640a51c656
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-26 15:57:39 -0300

    for

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,20 @@
 fun main() {
-    val s = readlnOrNull() // read Line
-    println(s)
+    val name = "Samuel"
+
+    println("==== Estrutura de Repetição for: Iterando uma String ($name) ====")
+    for (char in name) {
+        print(char)
+    }
+
+    println("\n\n==== Estrutura de Repetição for: Contando de 1 a 20 de 2 em 2 ====")
+    for (i in 1..20 step 2) {
+        print("$i ")
+    }
+
+    println("\n\n==== Estrutura de Repetição for: Contando ao contrario, de 20 até 0 ====")
+    for (i in 20 downTo 0) {
+        print("$i ")
+    }
+
+
 }
