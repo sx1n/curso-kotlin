3432e36b7ea00a743df83cc24318053b3b363edc
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-26 16:48:12 -0300

    while

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,10 @@
 fun main() {
-    val name = "Samuel"
 
-    println("==== Estrutura de Repetição for: Iterando uma String ($name) ====")
-    for (char in name) {
-        print(char)
+    var i = 0
+
+    while (true) {
+        println("Eu sou o while")
+        ++i
+        if (i > 1_000_000) break
     }
-
-    println("\n\n==== Estrutura de Repetição for: Contando de 1 a 20 de 2 em 2 ====")
-    for (i in 1..20 step 2) {
-        print("$i ")
-    }
-
-    println("\n\n==== Estrutura de Repetição for: Contando ao contrario, de 20 até 0 ====")
-    for (i in 20 downTo 0) {
-        print("$i ")
-    }
-
-
 }
