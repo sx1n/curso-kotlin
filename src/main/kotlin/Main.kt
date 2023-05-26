30f616bccbd1effa40b2f1e3c166eb7956611ac3
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-26 19:56:46 -0300

    try/catch/finally

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 fun main() {
-    var s: String? = null // o ? permite o uso de nulos
-    println(s?.length) // o ? protege de valores nulos
-    println(s!!.length) // !! não protege de valores nulos
+    try {
+        var s: String? = null // o ? permite o uso de nulos
+        println(s!!.length) // !! não protege de valores nulos
+    } catch (e: NullPointerException) {
+        println("Variável nula")
+    } catch (e: Exception) {
+        println("Capturando qualquer tipo de exceção")
+    } finally {
+        // ... codigo
+        // Esse bloco de codigo sempre vai ser executado, independente de qualquer erro ou sucesso
+        // Bloco Opcional
+    }
 }
