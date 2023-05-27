c1e64c92f453184d2ea8963db37ac93f0879b3e6
sx1n <samuellaurindodelima2004@gmail.com>
2023-05-27 20:13:40 -0300

    Classes: Construtor primario e secundario

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,19 @@
 class Pessoa(var nome: String, var anoDeNascimento: Int) {
+
+    var doc: String? = null
+
+    // Construtor Secundario que chama o Primario
+    constructor(nome: String, anoDeNascimento: Int, doc: String): this(nome, anoDeNascimento) {
+        this.doc = doc
+    }
+
     fun comer() {}
 
     fun falar() {}
 }
 
 fun main() {
-    val pessoa: Pessoa = Pessoa("Samuel", 2004)
+    val pessoa: Pessoa = Pessoa("Samuel", 2004, "cpf")
 
     println(pessoa.toString()) // igual Java
 }
