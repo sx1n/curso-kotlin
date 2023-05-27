5d4c0e279e4ad05d3a9d046e089367148c08fa26
sx1n <samuellaurindodelima2004@gmail.com>
2023-05-27 20:06:30 -0300

    Classes

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,16 +1,11 @@
+class Pessoa(var nome: String, var anoDeNascimento: Int) {
+    fun comer() {}
+
+    fun falar() {}
+}
+
 fun main() {
-    valores(false)
+    val pessoa: Pessoa = Pessoa("Samuel", 2004)
 
-    update()
-}
-
-// Any pode ser qualquer tipo do Kotlin, tem o mesmo comportamento de um Generic
-// Unit é semelhante ao void do Java e funções sem retorno são inferidas como Unit
-fun valores(arg: Any): Unit {
-
-}
-
-// Dificil de explicar, mas é quase a mesma ideia de interface
-fun update(): Nothing {
-    TODO("Não foi implementado")
+    println(pessoa.toString()) // igual Java
 }
