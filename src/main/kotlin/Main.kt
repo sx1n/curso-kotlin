c59b11fb9c7cf28b8310c86beddee0e06b522987
sx1n <samuellaurindodelima2004@gmail.com>
2023-05-27 17:03:10 -0300

    funcoes: generics

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,18 +1,16 @@
-import kotlin.reflect.typeOf
-
 fun main() {
-    println(media(1f, 2f, 3f, 44.5f, 8f))
+    println(media(1f, 2f, 3f, 44.5f, 8f, "", false))
 }
 
-fun media(vararg notas: Float): Float {
+fun <T, J> media(abc: J, vararg notas: T): Float {
     var soma = 0f
 
-    // println("$notas \ntipo de notas: ${notas::class.java.simpleName}")
-
-    // notas virá como um Float[]
     for (nota in notas) {
-        soma += nota
+        if (nota is Float) {
+            soma += nota
+        }
     }
 
+    // A média retornada não é real, pois tem valores que não são numeros de fato
     return soma / notas.size
 }
