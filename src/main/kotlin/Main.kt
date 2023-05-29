f64a9f6a1ea3609945a44e208200eafdcd0c46c9
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-29 18:09:41 -0300

    Classes: Data Class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,46 @@
-enum class Prioridade(val value: Int) {
-    Baixa(1) {
-        override fun toString(): String {
-            return "Prioridade baixa: $value"
-        }
-    },
-    Media(5), Alta(10)
+fun main() {
+    val f1: Forma = Forma(10, 8)
+    val f2: Forma = Forma(10, 8)
+
+    // f2 = f1 // aponta o f2 para o mesmo endereço de memoria pra f1
+
+    println(f1.equals(f2))
+    println()
+    println(f1.toString())
+    println(f1.hashCode())
+    println("-------------")
+    println(f2.toString())
+    println(f2.hashCode())
+
+    var f3: FormaData = FormaData(11, 9)
+    val f4: FormaData = FormaData(11, 9)
+    println()
+    println(f3.equals(f4))
+    println(f3.toString())
+    println(f3.hashCode())
+    println("----------")
+    println(f4.toString())
+    println(f4.hashCode())
+
+    f3 = f4.copy() // faz um copia das propriedades de f4 para f3, sem apontar na memoria
+
 }
 
-fun main() {
-    for (prioridade in Prioridade.values()) {
-        println("${prioridade}:${prioridade.value}")
+class Forma(val a: Int, val b: Int) {
+    override fun equals(other: Any?): Boolean {
+        return if(other is Forma) {
+            (other.a == this.a && other.b == this.b)
+        } else {
+            false
+        }
+    }
+
+    override fun hashCode(): Int {
+        return super.hashCode()
+    }
+
+    override fun toString(): String {
+        return "$a + $b"
     }
 }
+data class FormaData(val a: Int, val b: Int)
