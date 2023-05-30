4c4cd7e6fe5f13821b545684dc29e3352ab7945a
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-30 14:59:02 -0300

    Classes: Classes Abstratas

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,47 +1,19 @@
-class Matematica {
-
-    // Todas as estruturas abaixo de comportam como `static`
-    // companion object pode, mas não precisa de nome
-    // Só pode haver 1 companion object na classe
-    // é possível acessar seus valores sem passar o nome definido na estrutura
-    companion object Valores {
-        val PI = 3.1415
-        fun teste() {}
-
-        init {
-            println("Serei chamado apenas 1 vez")
-        }
+abstract class Mamifero(val nome: String) {
+    fun acordar() {
+        println("Acordei.")
+    }
+    fun dormir() {
+        println("dormi")
     }
 
-    // Bom para agrupar valores estaticos
-    // Tem que referenciar o nome do object para acessar os seus valores
-    object obj1 {
-        val PI = 3.1415
-        fun teste() {}
+    abstract fun falar()
+}
 
-        init {
-            println("Fui acessado no obj1")
-        }
-    }
-    object obj2 {
-        val PI = 3.1415
-        fun teste() {}
+class Cachorro(nome: String) : Mamifero(nome) {
+    override fun falar() {
 
-        init {
-            println("Fui acessado no obj2")
-        }
     }
 }
 fun main() {
-    Matematica.PI
-    Matematica.Valores.PI // também é possível acessar metodos e atributos estaticos através do nome do companion object
-
-    Matematica.teste()
-    Matematica.Valores.teste()
-
-    Matematica.obj1.PI
-    Matematica.obj1.teste()
-
-    Matematica.obj2.PI
-    Matematica.obj2.teste()
+    val m = Cachorro("Joao")
 }
