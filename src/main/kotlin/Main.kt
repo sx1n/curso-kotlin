0337e4c9e76e84bc813b09e2efb9de4c0e607c4b
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-30 20:28:53 -0300

    Lists: any() e count()

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,39 @@
+fun geraDados(): List<Receita> {
+    return listOf(
+        Receita(
+            "Lasanha", 1200,
+            listOf(
+                Ingrediente("Presunto", 5),
+                Ingrediente("Queijo", 10),
+                Ingrediente("Molho de tomate", 2),
+                Ingrediente("Manjerição", 3)
+            )
+        ),
+        Receita("Panqueca", 500),
+        Receita("Omelete", 200),
+        Receita("Parmegiana", 700),
+        Receita("Sopa de feijão", 300),
+        Receita(
+            "Hamburguer", 2000,
+            listOf(
+                Ingrediente("Pão", 1),
+                Ingrediente("Hamburguer", 3),
+                Ingrediente("Queijo", 1),
+                Ingrediente("Catupiry", 1),
+                Ingrediente("Bacon", 3),
+                Ingrediente("Alface", 1),
+                Ingrediente("Tomate", 1)
+            )
+        )
+    )
+}
+
+data class Receita(val nome: String, val calorias: Int, val ingredientes: List<Ingrediente> = listOf())
+data class Ingrediente(val nome: String, val quantidade: Int)
+
 fun main() {
-    // Map é uma lista chave-valor com chaves únicas
+    val data = geraDados()
 
-    val map1: Map<String, String> = mapOf(Pair("França", "Paris"), Pair("Alemanha", "Berlim"))
-    var map2: MutableMap<String, String> = mutableMapOf() // map mútavel
-
-    map2.putAll(map1)
-    map2.remove("Alemanha")
-    println(map2) // {França=Paris} PRA CIMAAAAA
-
+    println("Tenho dados? ${if (data.any()) "Sim" else "Não"}") // verifica se há algum elemento
+    println("Tenho ${data.count()} elementos") // retorna a quantidade de elementos
 }
