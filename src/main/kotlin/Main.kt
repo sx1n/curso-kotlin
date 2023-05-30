135a2a86047c063f8c3de62a31c1785c862771c8
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-30 20:02:19 -0300

    Map

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 fun main() {
-    // Set é uma lista desordenada e não permite valores duplicados
+    // Map é uma lista chave-valor com chaves únicas
 
-    var set1: Set<String> = setOf("Paris", "Berlim", "Madrid", "Madrid")
-    val set2: MutableSet<String> = mutableSetOf() // Set mútavel
+    val map1: Map<String, String> = mapOf(Pair("França", "Paris"), Pair("Alemanha", "Berlim"))
+    var map2: MutableMap<String, String> = mutableMapOf() // map mútavel
 
-    set2.addAll(set1)
-    set2.remove("Madrid")
-    println(set2)
+    map2.putAll(map1)
+    map2.remove("Alemanha")
+    println(map2) // {França=Paris} PRA CIMAAAAA
 
 }
