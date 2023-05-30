b27991e0324ea3f05dd60b8b29329bd7fd2bd28f
Samuel Laurindo <samuellaurindodelima2004@gmail.com>
2023-05-30 14:37:03 -0300

    Classes: object e companion object É parecido com o conceito de static do Java

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,33 +1,47 @@
-open class Eletronico(private var marca: String) {
-    private fun corrente(ativo: Boolean) {}
-    fun ligar() {
-        corrente(true)
-    }
-    // ´open` também permite que a função da classe pai seja sobrescrita na classe filha, override
-    open fun desligar() {
-        corrente(false)
-    }
-}
+class Matematica {
 
-class Computador(marca: String) : Eletronico(marca) {
-    fun save() {}
+    // Todas as estruturas abaixo de comportam como `static`
+    // companion object pode, mas não precisa de nome
+    // Só pode haver 1 companion object na classe
+    // é possível acessar seus valores sem passar o nome definido na estrutura
+    companion object Valores {
+        val PI = 3.1415
+        fun teste() {}
 
-    override fun desligar() {
-        save()
-        super.desligar() // chama o desligar da classe pai
+        init {
+            println("Serei chamado apenas 1 vez")
+        }
     }
 
+    // Bom para agrupar valores estaticos
+    // Tem que referenciar o nome do object para acessar os seus valores
+    object obj1 {
+        val PI = 3.1415
+        fun teste() {}
 
-    // sobrecarga, Overload
-    fun save(a: String) {}
-    fun save(a: Int) {}
-    fun save(a: Double) {}
-    fun save(a: Float) {}
+        init {
+            println("Fui acessado no obj1")
+        }
+    }
+    object obj2 {
+        val PI = 3.1415
+        fun teste() {}
 
+        init {
+            println("Fui acessado no obj2")
+        }
+    }
 }
 fun main() {
-    val c = Computador("a")
+    Matematica.PI
+    Matematica.Valores.PI // também é possível acessar metodos e atributos estaticos através do nome do companion object
 
-    c.ligar()
-    c.desligar()
+    Matematica.teste()
+    Matematica.Valores.teste()
+
+    Matematica.obj1.PI
+    Matematica.obj1.teste()
+
+    Matematica.obj2.PI
+    Matematica.obj2.teste()
 }
